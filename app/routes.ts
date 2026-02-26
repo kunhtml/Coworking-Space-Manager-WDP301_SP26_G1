3a82087d58379a1a77c4bb670dc9c5b81554f822
kunhtml <94962460+kunhtml@users.noreply.github.com>
2026-02-26 23:23:34 +0700

    Delete routes.ts

--- app/routes.ts
+++
@@ -1,9 +0,0 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
-
-export default [
-  index("routes/home.tsx"),
-  route("spaces", "routes/spaces.tsx"),
-  route("dashboard", "routes/dashboard.tsx"),
-  route("menu", "routes/menu.tsx"),
-  route("login", "routes/login.tsx"),
-] satisfies RouteConfig;
