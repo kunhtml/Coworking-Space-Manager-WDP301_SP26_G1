74cfdd6aeded7bdb656f893783b805cda8896f7e
kunhtml <94962460+kunhtml@users.noreply.github.com>
2026-03-10 14:25:45 +0700

    remove

--- app/routes.js
+++
@@ -1,9 +0,0 @@
-import { index, route } from "@react-router/dev/routes";
-
-export default [
-  index("routes/home.jsx"),
-  route("spaces", "routes/spaces.jsx"),
-  route("dashboard", "routes/dashboard.jsx"),
-  route("menu", "routes/menu.jsx"),
-  route("login", "routes/login.jsx"),
-];
